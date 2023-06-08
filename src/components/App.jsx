d5d04acfd41df52ff1af55f164f29466b2ac7939
Aleksandra <a.walczak1984@gmail.com>
2023-06-08 11:35:41 +0200

    change -comments in add.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import user from './Profile/user.json';
 import Profile from './Profile/Profile'; // Import komponentu Profile
 
-import data from './Statistics/data.json';
-import Statistics from './Statistics/Statistics'; // Import komponentu Statistics
+// import data from './Statistics/data.json';
+// import Statistics from './Statistics/Statistics'; // Import komponentu Statistics
 
-import friends from './FriendList/friends.json';
-import FriendList from './FriendList/FriendList'; // Import komponentu FriendList
+// import friends from './FriendList/friends.json';
+// import FriendList from './FriendList/FriendList'; // Import komponentu FriendList
 
-import transactions from './TransactionHistory/transactions.json';
-import TransactionHistory from './TransactionHistory/TransactionHistory'; // Import komponentu TransactionHistory
+// import transactions from './TransactionHistory/transactions.json';
+// import TransactionHistory from './TransactionHistory/TransactionHistory'; // Import komponentu TransactionHistory
 
 export const App = () => {
   return (
@@ -29,9 +29,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics stats={data} />
+      {/* <Statistics stats={data} />
       <FriendList friends={friends} />;
-      <TransactionHistory items={transactions} />
+      <TransactionHistory items={transactions} /> */}
     </div>
   );
 };
